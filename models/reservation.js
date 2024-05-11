86e6403d2f3a0cde04ea7e1e4ff5c9e29bc7bec6
Syan <105128575+Syan310@users.noreply.github.com>
2024-05-10 22:18:36 -0400

    Update reservation.js

--- models/reservation.js
+++ models/reservation.js
@@ -1,1 +1,14 @@
+const { MongoClient } = require('mongodb');
 
+class Reservation {
+    constructor(db) {
+        this.collection = db.collection('reservations');
+    }
+
+    async create(data) {
+        const result = await this.collection.insertOne(data);
+        return result;
+    }
+}
+
+module.exports = Reservation;
